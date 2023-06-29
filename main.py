6afb9e90e524262a58a0988a1b692e1fb6e43681
Nguyen Xuan Phong <nguyenphong011978@gmail.com>
2023-06-29 14:46:16 +0700

    creat link api

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/api/v1/<station>/<date>")
+def solve(station, date):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
+    return {
+        "station": station,
+        "date": date,
+        "temperature": temperature}
+app.run(debug=True)
+print("OK")
