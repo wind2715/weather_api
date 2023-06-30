3802f163ae5c4de233ee94c674df19f50095a160
Nguyen Xuan Phong <nguyenphong011978@gmail.com>
2023-06-30 08:57:47 +0700

    show tabel contain station

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import pandas as pd
 
 app = Flask(__name__)
 
-
+stations = pd.read_csv("data_small/stations.txt", skiprows=17)
+stations = stations[["STAID","STANAME                                 "]]
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data = stations.to_html())
 
 
 @app.route("/api/v1/<station>/<date>")
